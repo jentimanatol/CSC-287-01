73478ea2ddaa831f15ebc31b9d356e2f11c5bd1d
Anatol <jentimanatol@gmail.com>
2024-11-07 22:11:35 -0500

    save
    save

---
+++ python-aliens_original/settings.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = self.ship_speed * (2/3)
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
