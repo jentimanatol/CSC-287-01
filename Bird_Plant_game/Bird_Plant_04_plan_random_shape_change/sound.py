99c576be2ec8f99c2b86d34a320b3677c9850fc2
jentimanatol <jentimanatol@gmail.com>
2024-12-21 02:38:28 -0500

    Auto Commit CSC-287

--- Bird_Plant_game/Bird_Plant_04_plan_random_shape_change/sound.py
+++ Bird_Plant_game/Bird_Plant_04_plan_random_shape_change/sound.py
@@ -8,7 +8,7 @@ class Sound:
     def __init__(self):
         """Initialize the game's sound effects."""
         pygame.mixer.init()
-        self.explosion_sound = pygame.mixer.Sound('C:\\BHCC\\python\\CSC-287-01\\starter_game\\assets\\sounds\\explosion.flac')
+        self.explosion_sound = pygame.mixer.Sound('assets\\sounds\\explosion.flac')
 
     def play_explosion(self):
         """Play the explosion sound."""
